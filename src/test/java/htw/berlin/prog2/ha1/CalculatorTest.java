526210cd3985820286a72e5f96d48884f09a68a8
Elif <Elif.Kurtcuoglu@student.htw-berlin.de>
2025-10-15 14:34:35 +0200

    zusätzlicher Test

--- src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
+++ src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
@@ -90,5 +90,22 @@ class CalculatorTest {
 
 
     //TODO hier weitere Tests erstellen
+    @Test
+    @DisplayName("should calculate multiplication correctly")
+    void testPositiveMultiplication() {
+        Calculator calc = new Calculator();
+
+        calc.pressDigitKey(5);
+        calc.pressBinaryOperationKey("*");
+        calc.pressDigitKey(4);
+        calc.pressEqualsKey();
+
+        String expected = "20";
+        String actual = calc.readScreen();
+
+        assertEquals(expected, actual);
+
+    }
+
 }
 
